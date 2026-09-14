512c2eaa72cf4f2624ff218c6861d39f8ff16402
Maria Eduarda Albuquerque <dudinhazilda17@gmail.com>
2026-09-14 18:36:24 -0300

    Delete PerfilEConfiguracoesScreen directory

--- PerfilEConfiguracoesScreen/index.js
+++
@@ -1,6 +0,0 @@
-import { Redirect } from 'expo-router';
-
-export default function Index() {
-  // Redireciona a tela inicial diretamente para a sua tela de Perfil
-  return <Redirect href="/Screens/Perfil/perfil" />;
-}
